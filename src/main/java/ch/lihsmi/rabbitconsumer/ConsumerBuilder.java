f717a2c2bd730976941af1aa7a36e7a94a6bdccc
Michael Lihs <lihs@punkt.de>
2016-04-19 11:57:36 +0200

    [TASK] `ConsumerBuilder` now can inject given receiver

--- src/main/java/ch/lihsmi/rabbitconsumer/ConsumerBuilder.java
+++ src/main/java/ch/lihsmi/rabbitconsumer/ConsumerBuilder.java
@@ -11,11 +11,16 @@ public class ConsumerBuilder {
     ConnectionFactory connectionFactory;
 
     private boolean withFaultyReceiver = false;
+
     private int runtime = 0;
+
+    private Receiver injectedReceiver;
 
     public Consumer build(String consumerName, String routingKey, String queueName) {
         Receiver receiver;
-        if (withFaultyReceiver) {
+        if (this.injectedReceiver != null) {
+            receiver = this.injectedReceiver;
+        } else if (withFaultyReceiver) {
             receiver = new FaultyReceiver(consumerName);
         } else if (runtime > 0) {
             receiver = new LongRunningReceiver(consumerName, runtime);
@@ -32,6 +37,11 @@ public class ConsumerBuilder {
 
     public ConsumerBuilder withRuntime(int runtime) {
         this.runtime = runtime;
+        return this;
+    }
+
+    public ConsumerBuilder withReceiver(Receiver receiver) {
+        this.injectedReceiver = receiver;
         return this;
     }
 
